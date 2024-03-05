a3ba3ab26534c8d23b24fb793f3f8bd06b365fb7
dxthki <thinh.kieu@tuni.fi>
2024-03-05 18:47:42 +0200

    uibuild

---
+++ backend/index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const app = express(); 
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(express.static('dist'))
+app.use(cors());
+app.use(express.json());
+app.use(morgan(function (tokens, req, res) {
+    if (tokens.method(req, res) === 'POST') {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            JSON.stringify(req.body)
+        ].join(' ')
+    }
+    else {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms'
+          ].join(' ')
+    }
+}));
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+    return Math.floor(Math.random() * 100000000000000000);
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const currentDate = new Date();
+    const currentTime = currentDate.toString();
+
+    const info = `Phonebook has info for ${persons.length} people <br/> ${currentTime}`;
+    response.send(info);
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    const personExists = persons.find(person => person.name === body.name)
+    if (personExists) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+    }
+  
+    const person = {
+      name: body.name, 
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
